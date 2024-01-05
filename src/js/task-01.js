645b98399f1f0e8b25370f8d993db811d843c508
DmitryKuprienko <101987031+DmitryKuprienko@users.noreply.github.com>
2024-01-06 00:17:26 +0200

    треба перевіряти що змінилось

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,9 +1,10 @@
-const itemRef = document.querySelectorAll(".item")
-console.log(itemRef)
+const itemRef = document.querySelectorAll('.item');
+console.log(itemRef);
 
-console.log(`Number of categories=>`, itemRef.length)
+console.log(`Number of categories=>`, itemRef.length);
 itemRef.forEach(element => {
-    console.log("Category=>", element.firstElementChild.textContent)
-    console.log("Elements=>", element.lastElementChild.childElementCount)
+  console.log('Category=>', element.firstElementChild.textContent);
+  console.log('Elements=>', element.lastElementChild.childElementCount);
 });
 // console.log([])
+$env: PATH += ';C:Program FilesGit\bin';
