04b8a59565028f87d9dde24dd9919a7ecf1f5aec
DmitryKuprienko <101987031+DmitryKuprienko@users.noreply.github.com>
2024-02-22 10:30:08 +0200

    test push

--- src/js/task-05.js
+++ src/js/task-05.js
@@ -3,7 +3,7 @@ const formRef = document.querySelector('#name-input');
 const outpuRef = document.querySelector('#name-output');
 // console.dir(outpuRef);
 
-// console.dir(formRef);
+console.dir(formRef);
 
 formRef.addEventListener('input', event => {
 
